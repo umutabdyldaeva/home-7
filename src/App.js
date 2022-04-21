6470bf43a230d4d8a4468f0648b35002775d4bdb
umutabdyldaeva <umut.abdyldaeva91@gmail.com>
2022-04-21 15:40:50 +0300

    home-8

--- src/App.js
+++ src/App.js
@@ -2,24 +2,46 @@ import Comment from './components/Comment';
 
 import './App.css';
 
-const comment = {
+const comment = [
+{
   date: new Date(),
   text: 'I hope you enjoy learning React!',
   author: {
     name: 'Hello Kitty',
     avatarUrl: 'http://placekitten.com/g/64/64'
-  },
-};
+  }
+},
+{
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+},
+{
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+}
 
+]
 
 function App() {
   return (
-    <div>
-      <Comment author = {comment.author}
-               text = {comment.text}
-              date = {comment.date}/>
-     
-     
+    <div className='App'>
+      {comment.map((el) => {
+        return(
+          <Comment key = {el.id}
+              author = {el.author}
+              text = {el.text}
+              date = {el.date}/>
+        )
+      })
+      } 
     </div>
   );
 }
